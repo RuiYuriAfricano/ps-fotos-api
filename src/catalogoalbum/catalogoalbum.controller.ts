6f596f2c74668fd46a66aee139e9d1072ede5819
RuiYuriAfricano <yuriafricano03@gmail.com>
2023-11-20 04:25:12 +0100

    feat: add inviteUsers

--- src/catalogoalbum/catalogoalbum.controller.ts
+++ src/catalogoalbum/catalogoalbum.controller.ts
@@ -15,7 +15,7 @@ import { ListCatalogoFotosDto } from './dto/listCatalogoFotosDto';
 
 @Controller('catalogoalbum')
 export class CatalogoAlbumController {
-  constructor(private catalogoAlbumService: CatalogoAlbumService) {}
+  constructor(private catalogoAlbumService: CatalogoAlbumService) { }
 
   @Post()
   add(@Body() data: AddCatalogoAlbumDto) {
@@ -46,4 +46,25 @@ export class CatalogoAlbumController {
   getAlbumPhotos(@Body() data: ListCatalogoFotosDto) {
     return this.catalogoAlbumService.listarFotos(data);
   }
+
+  @Post('addUsers')
+  async addUsersCatalogo(@Body() requestBody: { codutilizadores: number[], codalbum: number }) {
+    // Verifica se codutilizadores é uma array válida e não é vazia
+    if (requestBody.codutilizadores && requestBody.codutilizadores.length) {
+      const catalogoAlbums = [];
+
+      for (let i = 0; i < requestBody.codutilizadores.length; i++) {
+        const catalogoalbum = await this.catalogoAlbumService.addUserCatalogo(requestBody.codutilizadores[i], requestBody.codalbum);
+        catalogoAlbums.push(catalogoalbum);
+      }
+
+      return catalogoAlbums;
+    } else {
+      // Trate o caso em que codutilizadores é undefined, null ou vazio
+      return { error: 'A lista de utilizadores é inválida ou vazia.' };
+    }
+  }
+
+
+
 }
